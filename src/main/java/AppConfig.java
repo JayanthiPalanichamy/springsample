5a15ac64c97c5e3a34ac2b2a82cee7f26109c52e
Jayanthi <jayanthi.palanichamy1997@gmail.com>
2019-03-26 23:40:56 +0530

    use java configuration with autowiring repository

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -3,20 +3,17 @@ import com.thoughtWorks.repository.HibernateCustomerRepositoryImpl;
 import com.thoughtWorks.service.CustomerService;
 import com.thoughtWorks.service.CustomerServiceImpl;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan({"com.thoughtWorks"})
 public class AppConfig {
-
-    @Bean(name = "customerRepository")
-    public CustomerRepository getCustomerRepository() {
-        return new HibernateCustomerRepositoryImpl();
-    }
 
     @Bean(name = "customerService")
     public CustomerService getCustomerService() {
 
-        CustomerServiceImpl customerService = new CustomerServiceImpl(getCustomerRepository());
+        CustomerServiceImpl customerService = new CustomerServiceImpl();
         return customerService;
     }
 }
--- src/main/java/com/thoughtWorks/service/CustomerServiceImpl.java
+++ src/main/java/com/thoughtWorks/service/CustomerServiceImpl.java
@@ -2,18 +2,14 @@ package com.thoughtWorks.service;
 
 import com.thoughtWorks.model.Customer;
 import com.thoughtWorks.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class CustomerServiceImpl implements CustomerService{
 
-    public CustomerServiceImpl(CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
-    }
-
+    @Autowired
     private CustomerRepository customerRepository;
-
-
 
     public List<Customer> findAll() {
         return customerRepository.findAll();
