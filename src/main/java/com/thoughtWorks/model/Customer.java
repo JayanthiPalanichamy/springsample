a73470d9c020356602d5425e122c7bda1e5f2c90
Jayanthi <jayanthi.palanichamy1997@gmail.com>
2019-03-26 22:11:46 +0530

    create customer model class

---
+++ src/main/java/com/thoughtWorks/model/Customer.java
@@ -0,0 +1,27 @@
+package com.thoughtWorks.model;
+
+public class Customer {
+    String firstName;
+    String lastName;
+
+
+    public Customer() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
