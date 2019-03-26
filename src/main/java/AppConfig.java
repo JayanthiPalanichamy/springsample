d3a98339348dad3cbe6ef4a3a2f17a6e29f9b6a2
Jayanthi <jayanthi.palanichamy1997@gmail.com>
2019-03-26 23:43:18 +0530

    use java configuration with complete autowiring

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,8 +1,3 @@
-import com.thoughtWorks.repository.CustomerRepository;
-import com.thoughtWorks.repository.HibernateCustomerRepositoryImpl;
-import com.thoughtWorks.service.CustomerService;
-import com.thoughtWorks.service.CustomerServiceImpl;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +5,4 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan({"com.thoughtWorks"})
 public class AppConfig {
 
-    @Bean(name = "customerService")
-    public CustomerService getCustomerService() {
-
-        CustomerServiceImpl customerService = new CustomerServiceImpl();
-        return customerService;
-    }
 }
--- src/main/java/com/thoughtWorks/service/CustomerServiceImpl.java
+++ src/main/java/com/thoughtWorks/service/CustomerServiceImpl.java
@@ -3,9 +3,11 @@ package com.thoughtWorks.service;
 import com.thoughtWorks.model.Customer;
 import com.thoughtWorks.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service("customerService")
 public class CustomerServiceImpl implements CustomerService{
 
     @Autowired
