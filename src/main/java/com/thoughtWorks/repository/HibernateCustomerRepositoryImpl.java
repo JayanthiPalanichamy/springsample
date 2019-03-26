7de106cb8c4cb209d41b60ff92cc1b827033da0d
Jayanthi <jayanthi.palanichamy1997@gmail.com>
2019-03-26 23:01:35 +0530

    use annotation to autowire

--- src/main/java/com/thoughtWorks/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/com/thoughtWorks/repository/HibernateCustomerRepositoryImpl.java
@@ -1,10 +1,12 @@
 package com.thoughtWorks.repository;
 
 import com.thoughtWorks.model.Customer;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository("customerRepository")
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
      public List<Customer> findAll() {
          List<Customer> customers = new ArrayList<>();
--- src/main/java/com/thoughtWorks/service/CustomerServiceImpl.java
+++ src/main/java/com/thoughtWorks/service/CustomerServiceImpl.java
@@ -2,25 +2,18 @@ package com.thoughtWorks.service;
 
 import com.thoughtWorks.model.Customer;
 import com.thoughtWorks.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service("customerService")
 public class CustomerServiceImpl implements CustomerService{
+
+    @Autowired
     private CustomerRepository customerRepository;
-
-    public CustomerServiceImpl() {
-
-    }
-
-    public CustomerServiceImpl(CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
-    }
 
     public List<Customer> findAll() {
         return customerRepository.findAll();
-    }
-
-    public void setCustomerRepository(CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
     }
 }
